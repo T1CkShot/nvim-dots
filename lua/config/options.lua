6c7765f5ab87ddad285f573290297aa020ea3817
t1ckshot <fatta.bashar@gmail.com>
2024-08-18 07:03:12 +0600

    lsp border

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local border = {
+  { "╭", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╮", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "╯", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╰", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+vim.lsp.with(vim.lsp.handlers.hover, { border = border })
